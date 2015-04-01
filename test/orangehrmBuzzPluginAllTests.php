2f4c31909ab353ec9d133771bf1446f0a2d3e113
damith <damith>
2015-04-01 19:39:56 +0000

    fixing an issue in buzz plugin unit tests

--- test/orangehrmBuzzPluginAllTests.php
+++ test/orangehrmBuzzPluginAllTests.php
@@ -3,7 +3,7 @@
 class orangehrmBuzzPluginAllTests {
 
     protected function setUp() {
-        
+
     }
 
     public static function suite() {
@@ -12,23 +12,15 @@ class orangehrmBuzzPluginAllTests {
 
 
         $suite->addTestFile(dirname(__FILE__) . '/model/dao/BuzzDaoTest.php');
-       $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzServiceTest.php');
-       $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzConfigServiceTest.php');
-       $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzTextParserServiceTest.php');
-       $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzCookieServiceTest.php');
-<<<<<<< .working
-       
-       // utility test cases
-       $suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzWebServiceHelperTest.php');
-       $suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzObjectBuilderTest.php');
-       
-=======
-       
-       // utility service test
-       $suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzWebServiceHelperTest.php');
-       $suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzObjectBuilderTest.php');
-       
->>>>>>> .merge-right.r63046
+        $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzServiceTest.php');
+        $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzConfigServiceTest.php');
+        $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzTextParserServiceTest.php');
+        $suite->addTestFile(dirname(__FILE__) . '/model/service/BuzzCookieServiceTest.php');
+           
+        // utility test cases
+        $suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzWebServiceHelperTest.php');
+        $suite->addTestFile(dirname(__FILE__) . '/model/utility/BuzzObjectBuilderTest.php');
+
         return $suite;
     }
 
