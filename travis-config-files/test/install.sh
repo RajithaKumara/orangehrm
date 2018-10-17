d416de9f62bc541feb53c63b35f3850d522e869c
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-17 13:40:15 +0530

    change create-test-db.php

--- travis-config-files/test/install.sh
+++ travis-config-files/test/install.sh
@@ -1,5 +1,8 @@
 composer install -d symfony/lib
 composer dump-autoload -o -d symfony/lib
 php installer/cli_install.php 0
+
+cat lib/confs/Conf.php
+
 php devTools/general/create-test-db.php $MYSQL_ROOT_PASSWORD
 
