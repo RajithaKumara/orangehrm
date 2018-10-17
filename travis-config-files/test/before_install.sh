858ede4eee99bc75a7fc0affaf5c1544bc1bc986
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-17 11:50:56 +0530

    Add docker container

--- travis-config-files/test/before_install.sh
+++ travis-config-files/test/before_install.sh
@@ -1,7 +1,14 @@
+set -ev
+
 uname -a
 phpunit --version
 mysqladmin -uroot status
 composer self-update
 sudo chmod 777 -R symfony/cache
 sudo chmod 777 -R symfony/log
-echo "USE mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
+
+sudo /etc/init.d/mysql stop
+
+docker pull $DB_IMAGE:$TAG
+docker run --name mysql80 -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d $DB_IMAGE:$TAG
+#echo "USE mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
