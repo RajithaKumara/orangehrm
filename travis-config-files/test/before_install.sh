7fa6f64ad405fda78e5df8fc55d53f61e7a9d31f
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-17 12:26:45 +0530

    Check mysql conn

--- travis-config-files/test/before_install.sh
+++ travis-config-files/test/before_install.sh
@@ -7,10 +7,11 @@ composer self-update
 sudo chmod 777 -R symfony/cache
 sudo chmod 777 -R symfony/log
 
-sudo /etc/init.d/mysql stop
+#sudo /etc/init.d/mysql stop
 
 docker pull $DB_IMAGE:$TAG
 docker run --name $DB_IMAGE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -p 3306:3306 -d $DB_IMAGE:$TAG
 
 docker ps
+mysql -uroot --host=127.0.0.1 --port=3306 -p root
 #echo "USE mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
