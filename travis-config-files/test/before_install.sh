d22776783560704f6a29520dbee9e0364adee4f6
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-21 15:06:16 +0530

    Change mysql auth plugin

--- travis-config-files/test/before_install.sh
+++ travis-config-files/test/before_install.sh
@@ -13,7 +13,11 @@ docker run --name $DB_IMAGE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -p 3306:
 # This sleep is need to start MySQL service
 sleep 15
 
-docker exec -it mysql sh -c "mysql -uroot -proot -e \"status;\""
-exp="UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user.User = 'root';FLUSH PRIVILEGES;"
-docker exec -it mysql sh -c "mysql -uroot -proot -e \"${exp}\""
-docker exec -it mysql sh -c "/etc/init.d/mysql restart"
+docker exec -it $DB_IMAGE sh -c "mysql -uroot -proot -e \"select user,host,plugin from mysql.user;\""
+query="UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='root';FLUSH PRIVILEGES;"
+docker exec -it $DB_IMAGE sh -c "mysql -uroot -proot -e \"${query}\""
+docker exec -it $DB_IMAGE sh -c "mysql -uroot -proot -e \"select user,host,plugin from mysql.user;\""
+#docker exec -it $DB_IMAGE sh -c "/etc/init.d/mysql restart"
+docker container restart $DB_IMAGE
+
+sleep 15
