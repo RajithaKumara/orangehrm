3078aeba51e9f4032327416c941eb7b3b43e5038
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-21 11:23:51 +0530

    Test in mysql 8.0

--- travis-config-files/test/before_install.sh
+++ travis-config-files/test/before_install.sh
@@ -12,3 +12,8 @@ docker run --name $DB_IMAGE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -p 3306:
 
 # This sleep is need to start MySQL service
 sleep 15
+
+docker exec -it mysql sh -c "mysql -uroot -proot -e \"status;\""
+exp="UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user.User = 'root';FLUSH PRIVILEGES;"
+docker exec -it mysql sh -c "mysql -uroot -proot -e \"${exp}\""
+docker exec -it mysql sh -c "/etc/init.d/mysql restart"
